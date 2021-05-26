fc69548d189cf840d01897a918c439e957ad724b
ifotn <rich@infrontofthenet.com>
2021-05-26 12:52:14 -0400

    Add project files.

---
+++ DotNetDrinks/Models/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetDrinks.Models
+{
+    public class Brand
+    {
+        // in ASP.NET, key fields should always be called either Id or {Model}Id
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int YearFounded { get; set; }
+    }
+}
